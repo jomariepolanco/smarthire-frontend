ce03ec37346d1a634967ebc94de128dae0e2acb2
jomariepolanco <jomariepolanco@gmail.com>
2021-01-16 16:07:17 -0500

    project container connected to store and passing down tasks to project card

--- src/Home/Containers/ProjectsContainer.js
+++ src/Home/Containers/ProjectsContainer.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react'
 import ProjectCard from '../Components/ProjectCard'
 import {Grid} from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import {getProjectsTasks} from '../../Redux/tasks/actions'
 
 class ProjectsContainer extends Component {
 
+    componentDidMount(){
+        const projectOneId = [...this.props.projects][0].id
+        const projectTwoId = [...this.props.projects][1].id
+        this.props.getProjectsTasks(projectOneId, projectTwoId)
+    }
+
     renderCandidateProject = () => {
         const candidate = [...this.props.projects].find(pro => pro.title === "Candidate")
+        const candidateTasks = [...this.props.tasks].filter(task => task.projectId === candidate.id)
         return(
             <>
             <h1>{candidate.title}</h1>
-            <ProjectCard />
+            <ProjectCard tasks={candidateTasks}/>
             </>
         )
     }
 
     renderClientProject = () => {
         const client = [...this.props.projects].find(pro => pro.title === "Client")
+        const clientTasks = [...this.props.tasks].filter(task => task.projectId === client.id)
         return(
             <>
             <h1>{client.title}</h1>
-            <ProjectCard />
+            <ProjectCard tasks={clientTasks}/>
             </>
         )
     }
@@ -46,4 +56,16 @@ class ProjectsContainer extends Component {
     }
 }
 
-export default ProjectsContainer;
+const msp = (state) => {
+    return {
+        tasks: state.tasks 
+    }
+}
+
+const mdp = (dispatch) => {
+    return {
+        getProjectsTasks: (projectOneId, projectTwoId) => dispatch(getProjectsTasks(projectOneId, projectTwoId))
+    }
+}
+
+export default connect(msp, mdp)(ProjectsContainer);
