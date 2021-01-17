face721344456a5536615f3239d5cd8fe8f3a880
jomariepolanco <jomariepolanco@gmail.com>
2021-01-16 21:34:20 -0500

    routing to candidate card works

--- src/Candidates/Components/CandidateList.js
+++ src/Candidates/Components/CandidateList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default class CandidateList extends Component {
 
@@ -7,9 +8,9 @@ export default class CandidateList extends Component {
     }
 
     renderCandyList = () => {
-        return [...this.props.candidates].map(candy => <div onClick={this.clickHandler}><h4 key={candy.id}>{candy.firstName} {candy.lastName}</h4></div>)
+        return [...this.props.candidates].map(candy => <NavLink to={`/candidates/${candy.id}`} key={candy.id}><h4 >{candy.firstName} {candy.lastName}</h4></NavLink>)
     }
-    
+
     render() {
         
         return (
--- src/Candidates/Containers/CandidateContainer.js
+++ src/Candidates/Containers/CandidateContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
 import { getCandidates } from '../../Redux/candidates/actions'
+import CandidateCard from '../Components/CandidateCard'
 import CandidateList from '../Components/CandidateList'
 import SearchForm from '../Components/SearchForm'
 
@@ -22,10 +24,24 @@ class CandidateContainer extends Component {
 
     render() {
         return (
-            <div>
-                <SearchForm candidates={this.props.candidates} submitHandler={this.searchFormSubmit}/>
-                <CandidateList candidates={this.state.searchedCandies} />
-            </div>
+            <Switch>
+                <Route path='/candidates/:id' render={({match}) => {
+                    let id = +match.params.id 
+
+                    let candidate = [...this.props.candidates].find(candy => candy.id === id)
+                    return <CandidateCard candidate={candidate} />
+                }} />
+
+                <Route path='/candidates' render={() => {
+                    return (
+                        <div>
+                            <SearchForm candidates={this.props.candidates} submitHandler={this.searchFormSubmit}/>
+                            <CandidateList candidates={this.state.searchedCandies} />
+                        </div>
+
+                    )
+                }} />
+            </Switch>
         )
     }
 }
