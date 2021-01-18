1dafb185567831630d1fda1c21918e9e2d42043c
jomariepolanco <jomariepolanco@gmail.com>
2021-01-17 20:24:33 -0500

    job card renders all job info

--- src/Jobs/Components/JobCard.js
+++ src/Jobs/Components/JobCard.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default class JobCard extends Component {
     render() {
+        const {job} = this.props
+        console.log(job)
         return (
             <div>
-                JobCard
+                <h1>{job.title}</h1>
+                <h4>{job.description}</h4>
+                <h5>${job.pay}.00/hour</h5>
+                <h5>Due: {job.dueDate}</h5>
+                <h4>Point of Contact:</h4>
+                <p>{job.poc.name}</p>
+                <p>{job.poc.email}</p>
+                <p>{job.poc.phoneNumber}</p>
             </div>
         )
     }
