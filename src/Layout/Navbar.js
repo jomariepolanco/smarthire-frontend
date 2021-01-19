f54e1f9eb531a7d95fcad1f543f435eb6baf503d
jomariepolanco <jomariepolanco@gmail.com>
2021-01-19 13:37:37 -0500

    uninstalled semantic

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,51 +1,41 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import { Button, Menu } from 'semantic-ui-react'
 import Logout from './Logout'
 
 export default class Navbar extends Component {
 
-    state = {}
-
-    hideFixedMenu = () => this.setState({fixed: false})
-    showFixedMenu = () => this.setState({fixed: true})
-
     render() {
-
-        const {fixed} = this.state
         return (
-            <Menu fixed={fixed ? 'top': null} inverted={!fixed} pointing={!fixed} secondary={!fixed} size='large'>
-                <Menu.Item position="left">
-                    <Button as={NavLink} to='/home'>
-                        Home 
-                    </Button>
-                </Menu.Item>
-                <Menu.Item position="left">
-                    <Button as={NavLink} to='/candidates'>
-                        Candidates
-                    </Button>
-                </Menu.Item>
-                <Menu.Item position="left">
-                    <Button as={NavLink} to='/clients'>
-                        Clients
-                    </Button>
-                </Menu.Item>
-                <Menu.Item position="left">
-                    <Button as={NavLink} to='/login'>
-                        Login
-                    </Button>
-                </Menu.Item>
-                <Menu.Item position="left">
-                    <Button as={NavLink} to='/signup'>
-                        Sign Up
-                    </Button>
-                </Menu.Item>
-                <Menu.Item position="left">
-                    <Button as={Logout}>
-                        Logout
-                    </Button>
-                </Menu.Item>
-            </Menu>
+            <ul>
+                    <li>
+                        <NavLink to='/home'>
+                            Home 
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/candidates'>
+                            Candidates
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/clients'>
+                            Clients
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/login'>
+                            Login
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/signup'>
+                            Sign Up
+                        </NavLink>
+                    </li>
+                    <li>
+                        <Logout />
+                    </li>
+            </ul>
         )
     }
 }
