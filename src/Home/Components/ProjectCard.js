c67c04e24364977ea401a721f8524b5fe6f67b56
jomariepolanco <jomariepolanco@gmail.com>
2021-01-19 11:53:59 -0500

    projects show percentage of tasks done each day

--- src/Home/Components/ProjectCard.js
+++ src/Home/Components/ProjectCard.js
@@ -6,10 +6,20 @@ export default class ProjectCard extends Component {
     renderTasks = () => {
         return [...this.props.tasks].map(task => <TaskCard key={task.id} task={task}/>)
     }
+
+    percentageOfTasksDone = () => {
+        let percentage = [...this.props.tasks].filter(task => task.archived).length / [...this.props.tasks].length 
+        if (percentage){
+            return <h5>{(percentage * 100).toFixed(0)}% tasks completed</h5>
+        } else {
+            return <h5>0% tasks completed</h5>
+        }
+    }
     
     render() {
         return (
             <div>
+                {this.percentageOfTasksDone()}
                 {this.renderTasks()}
             </div>
         )
